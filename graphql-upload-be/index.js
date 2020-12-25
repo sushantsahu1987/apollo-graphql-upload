da9f336d5f7318b433d1890df5d288e0e9a65f3f
Sushant Sahu <sushant87@gmail.com>
2020-12-25 12:40:36 +0530

    feat: code init

---
+++ graphql-upload-be/index.js
@@ -0,0 +1,42 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type File {
+    filename: String!
+    mimetype: String!
+    encoding: String!
+  }
+
+  type Query {
+    uploads: [File]
+  }
+
+  type Mutation {
+    singleUpload(file: Upload!): File!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    uploads: (parent, args) => {},
+  },
+  Mutation: {
+    singleUpload: (parent, args) => {
+      return args.file.then(file => {
+        //Contents of Upload scalar: https://github.com/jaydenseric/graphql-upload#class-graphqlupload
+        //file.createReadStream() is a readable node stream that contains the contents of the uploaded file
+        //node stream api: https://nodejs.org/api/stream.html
+        return file;
+      });
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
